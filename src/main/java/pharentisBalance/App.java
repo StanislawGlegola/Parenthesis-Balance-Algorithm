4546d50aec8ac092558a17e157e0c3b6a4e0fe9d
Stanislaw <sta.glegola@gmail.com>
2019-08-01 21:49:27 +0200

    Round bracket balancer

--- src/main/java/pharentisBalance/App.java
+++ src/main/java/pharentisBalance/App.java
@@ -1,51 +1,96 @@
 package pharentisBalance;
 
-interface BracketBalance {
-    boolean bracketBalance();
+interface ParenthesisBalance {
+    boolean parenthesisBalance();
+
 }
 
 public class App {
     public static void main(String[] args) {
-        String parenthisChain = "{}";
-        CurlyBrace balancer = new CurlyBrace(parenthisChain);
-        balancer.bracketBalance();
+        String curlyBracketsChain = "{}";
+        CurlyBrackets balanceCurlyBrackets = new CurlyBrackets(curlyBracketsChain);
+        balanceCurlyBrackets.parenthesisBalance();
+
+        String roundBracketsChain = "(())";
+        RoundBrackets balanceRoundBrackets = new RoundBrackets(roundBracketsChain);
+        balanceRoundBrackets.parenthesisBalance();
     }
 }
 
-class CurlyBrace implements BracketBalance {
+class CurlyBrackets implements ParenthesisBalance {
 
-    private String bracketChain;
+    private String curlyBracketChain;
 
-    public CurlyBrace(String bracketChain) {
-        this.bracketChain = bracketChain;
+    public CurlyBrackets(String curlyBracketChain) {
+        this.curlyBracketChain = curlyBracketChain;
     }
 
     @Override
-    public boolean bracketBalance() {
-        char[] temp = bracketChain.toCharArray();
+    public boolean parenthesisBalance() {
+        char[] temp = curlyBracketChain.toCharArray();
         int counter = 0;
 
-        if (bracketChain.length() % 2 == 0) {
-            for (int i = 0; i <= bracketChain.length() - 1; i++) {
+        if (curlyBracketChain.length() % 2 == 0) {
+            for (int i = 0; i <= curlyBracketChain.length() - 1; i++) {
                 if (temp[i] == '{') {
                     counter++;
                 } else if (temp[i] == '}') {
                     counter--;
                     if (counter < 0) {
-                        System.out.println("Kolejnosc bledna.");
+                        System.out.println("Curly Bracket order incorrect.");
                         return false;
                     }
                 }
             }
         } else {
-            System.out.println("Liczba znakow nie parzysta.");
+            System.out.println("Curly brackets number not even.");
             return false;
         }
         if (counter == 0) {
-            System.out.println("Liczba nawiasow poprawna.");
+            System.out.println("Number of CURLY brackets correct.");
             return true;
         } else {
-            System.out.println("Kolejnosc nawiasow bledna.");
+            System.out.println("Number of CURLY brackets incorrect.");
+            return false;
+        }
+    }
+}
+
+class RoundBrackets implements ParenthesisBalance {
+
+    private String roundBracketsChain;
+
+    RoundBrackets(String roundBracketsChain) {
+        this.roundBracketsChain = roundBracketsChain;
+    }
+
+    @Override
+    public boolean parenthesisBalance() {
+        char[] temp = roundBracketsChain.toCharArray();
+        Stack stack = new Stack(20);
+
+        if (roundBracketsChain.length() % 2 == 0) {
+            for (int i = 0; i <= roundBracketsChain.length() - 1; i++) {
+                if (temp[i] == '(') {
+                    stack.push(1);
+                } else if (temp[i] == ')') {
+                    if (stack.stackIsEmpty()) {
+                        System.out.println("Round brackets order incorrect.");
+                        return false;
+                    } else {
+                        stack.pop();
+                    }
+                }
+            }
+        } else {
+            System.out.println("Round brackets number not even");
+            return false;
+        }
+        if (stack.stackIsEmpty()) {
+            System.out.println("Number of ROUND brackets correct.");
+            return true;
+        } else {
+            System.out.println("Number of ROUND brackets incorrect");
             return false;
         }
     }
